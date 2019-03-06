50ceb348381d187f40679d03d72c478adee958b1
Sylvain Brouillat <sylvain.geotrend@gmail.com>
2019-03-06 14:49:47 +1100

    Add OpenAPI specification for /people/<id> and /people/<id>/favourite-food endpoints

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,10 +1,29 @@
-from flask import Flask, Blueprint, jsonify
+from flask import Flask, Blueprint, jsonify, request
 import backend.config as config
 from backend.people import people_v1
 from backend import db
 from backend.exceptions import BusinessException
 
+def add_cors_headers(response):
+    """ Enable CORS for any origin so that it can be requested for tools
+        like swagger UI from any domain.
+    """
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    if request.method == 'OPTIONS':
+        response.headers['Access-Control-Allow-Methods'] = 'DELETE, GET, POST, PUT'
+        headers = request.headers.get('Access-Control-Request-Headers')
+        if headers:
+            response.headers['Access-Control-Allow-Headers'] = headers
+    return response
+
 def create_app(test_config=None):
+    """ Flask application factory
+
+        Load applicaiton configuration, creates Flask instance
+        and attach some hook to it
+        The method is called using 'flask' command line with FLASK_APP=backend as environmnt
+        variable.
+    """
     app = Flask(__name__)
     app.config.from_mapping(
         mongodb={},
@@ -29,4 +48,7 @@ def create_app(test_config=None):
     db.init_app(app)
 
     app.register_blueprint(people_v1, url_prefix='/v1/people')
+    
+    app.after_request(add_cors_headers)
+
     return app
