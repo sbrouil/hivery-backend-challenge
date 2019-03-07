c058e447153a7dce665e7009037b066acadd6c2f
Sylvain Brouillat <sylvain.geotrend@gmail.com>
2019-03-08 10:56:39 +1100

    Add companies in loading process for companies to have their own collection.
    Previous model relying only on the people collection to get companies data was wrong cause if a company does not have any employee, it does not appear in database with this model. Having a collection of companies allow us to distinguish between 2 cases : companies with 0 employees, or unknown company

--- backend/db.py
+++ backend/db.py
@@ -28,16 +28,30 @@ def load_data_command():
     """
     print('Load data:')
     people = data.load_people()
+    companies = data.load_companies()
     companies_map = data.companies_map()
     people_map = data.people_map()
 
     db = get_db()
+    db.companies.drop()
     db.people.drop()
 
+    # inserting companies
+    inserted_count = 0
+    for c in companies:
+        doc = {
+            'index': c['index'],
+            'name': c['company']
+        }
+        print('Inserting company %s' % c['company'])
+        db.companies.insert_one(doc)
+    print('%d companies inserted' % inserted_count)
+
+    # inserting people
     inserted_count = 0
     for p in people:
         doc = data.prepare_person_document(p, companies_map, people_map)
-        print('Inserting %s' % p['name'])
+        print('Inserting person %s' % p['name'])
         db.people.insert_one(doc)
         inserted_count += 1
     print('%d people documents inserted' % inserted_count)
